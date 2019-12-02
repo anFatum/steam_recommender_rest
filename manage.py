da410d2694e0bc7d68b503354fe1ff2afc1c0c7c
Anastasia <ans.dudarenko@gmail.com>
2019-12-02 09:26:35 +0100

    Fix project name

--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'steam_recommend.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'steam_recommend_rest.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
--- steam_recommend_rest/wsgi.py
+++ steam_recommend_rest/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for steam_recommend project.
+WSGI config for steam_recommend_rest project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'steam_recommend.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'steam_recommend_rest.settings')
 
 application = get_wsgi_application()
