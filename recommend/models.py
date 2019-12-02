64ec332a81e874b709e21f856779f7931d645cf5
Anastasia <ans.dudarenko@gmail.com>
2019-12-02 09:04:30 +0100

    Downgrade string format to python3.6

--- recommend/models.py
+++ recommend/models.py
@@ -1,19 +1,18 @@
 from django.db import models
 
 
-# Create your models here.
 class SteamUser(models.Model):
     user_id = models.IntegerField(unique=True)
 
     def __str__(self):
-        return f"{self.user_id}"
+        return "{}".format(self.user_id)
 
 
 class SteamGame(models.Model):
     game_id = models.IntegerField(unique=True)
 
     def __str__(self):
-        return f"{self.game_id}"
+        return "{}".format(self.game_id)
 
 
 class Ownership(models.Model):
@@ -22,7 +21,7 @@ class Ownership(models.Model):
     game = models.ForeignKey(SteamGame, on_delete=models.DO_NOTHING)
 
     def __str__(self):
-        return f"{self.owner} played {self.game} for {self.time_played}"
+        return "{} played {} for {}".format(self.owner, self.game, self.time_played)
 
     class Meta:
         unique_together = (
